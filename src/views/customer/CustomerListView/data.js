13b740a9f05dc5835ca6cf9edf79d7db4b9f7525
Henrikas <henrikas@10speed.lt>
2020-11-15 23:09:48 +0200

    Initial commit.

---
+++ src/views/customer/CustomerListView/data.js
@@ -0,0 +1,264 @@
+import { v4 as uuid } from 'uuid';
+
+export default [
+  {
+    id: uuid(),
+    addresses: [
+      {
+        country: 'USA',
+        state: 'West Virginia',
+        city: 'Parkersburg',
+        street: '2849 Fulton Street'
+      },
+      {
+        country: 'USA',
+        state: 'West Virginia',
+        city: 'Parkersburg',
+        street: '2849 Fulton Street'
+      }
+    ],
+    avatarUrl: '/static/images/avatars/avatar_3.png',
+    createdAt: 1555016400000,
+    email: 'ekaterina.tankova@devias.io',
+    first_name: 'Ekaterina',
+    last_name: 'Tankova',
+    birthday: '1985-06-12',
+    phone: [
+      { type: 'mobile', number: '304-428-3097' },
+      { type: 'fax', number: '304-428-3097' }
+    ]
+  },
+  {
+    id: uuid(),
+    addresses: [
+      {
+        country: 'USA',
+        state: 'Bristow',
+        city: 'Iowa',
+        street: '1865  Pleasant Hill Road'
+      },
+      {
+        country: 'USA',
+        state: 'Bristow',
+        city: 'Iowa',
+        street: '1865  Pleasant Hill Road'
+      }
+    ],
+    avatarUrl: '/static/images/avatars/avatar_4.png',
+    createdAt: 1555016400000,
+    email: 'cao.yu@devias.io',
+    first_name: 'Cao',
+    last_name: 'Yu',
+    birthday: '1977-03-16',
+    phone: [
+      { type: 'mobile', number: '712-351-5711' },
+      { type: 'fax', number: '712-351-5711' }
+    ]
+  },
+  {
+    id: uuid(),
+    addresses: [
+      {
+        country: 'USA',
+        state: 'Georgia',
+        city: 'Atlanta',
+        street: '4894  Lakeland Park Drive'
+      },
+      {
+        country: 'USA',
+        state: 'Georgia',
+        city: 'Atlanta',
+        street: '4894  Lakeland Park Drive'
+      }
+    ],
+    avatarUrl: '/static/images/avatars/avatar_2.png',
+    createdAt: 1555016400000,
+    email: 'alexa.richardson@devias.io',
+    first_name: 'Alexa',
+    last_name: 'Richardson',
+    birthday: '1997-02-06',
+    phone: [
+      { type: 'mobile', number: '770-635-2682' },
+      { type: 'fax', number: '770-635-2682' }
+    ]
+  },
+  {
+    id: uuid(),
+    addresses: [
+      {
+        country: 'USA',
+        state: 'Ohio',
+        city: 'Dover',
+        street: '4158  Hedge Street'
+      },
+      {
+        country: 'USA',
+        state: 'Ohio',
+        city: 'Dover',
+        street: '4158  Hedge Street'
+      }
+    ],
+    avatarUrl: '/static/images/avatars/avatar_5.png',
+    createdAt: 1554930000000,
+    email: 'anje.keizer@devias.io',
+    first_name: 'Anje',
+    last_name: 'Keizer',
+    birthday: '1965-08-11',
+    phone: [
+      { type: 'mobile', number: '908-691-3242' },
+      { type: 'fax', number: '908-691-3242' }
+    ]
+  },
+  {
+    id: uuid(),
+    addresses: [
+      { country: 'USA', state: 'Texas', city: 'Dallas', street: '75247' },
+      { country: 'USA', state: 'Texas', city: 'Dallas', street: '75247' }
+    ],
+    avatarUrl: '/static/images/avatars/avatar_6.png',
+    createdAt: 1554757200000,
+    email: 'clarke.gillebert@devias.io',
+    first_name: 'Clarke',
+    last_name: 'Gillebert',
+    birthday: '1991-01-25',
+    phone: [
+      { type: 'mobile', number: '972-333-4106' },
+      { type: 'fax', number: '972-333-4106' }
+    ]
+  },
+  {
+    id: uuid(),
+    addresses: [
+      {
+        country: 'USA',
+        state: 'California',
+        city: 'Bakerfield',
+        street: '317 Angus Road'
+      },
+      {
+        country: 'USA',
+        state: 'California',
+        city: 'Bakerfield',
+        street: '317 Angus Road'
+      }
+    ],
+    avatarUrl: '/static/images/avatars/avatar_1.png',
+    createdAt: 1554670800000,
+    email: 'adam.denisov@devias.io',
+    first_name: 'Adam',
+    last_name: 'Denisov',
+    birthday: '1993-11-25',
+    phone: [
+      { type: 'mobile', number: '858-602-3409' },
+      { type: 'fax', number: '858-602-3409' }
+    ]
+  },
+  {
+    id: uuid(),
+    addresses: [
+      {
+        country: 'USA',
+        state: 'California',
+        city: 'Redondo Beach',
+        street: '2188  Armbrester Drive'
+      },
+      {
+        country: 'USA',
+        state: 'California',
+        city: 'Redondo Beach',
+        street: '2188  Armbrester Drive'
+      }
+    ],
+    avatarUrl: '/static/images/avatars/avatar_7.png',
+    createdAt: 1554325200000,
+    email: 'ava.gregoraci@devias.io',
+    first_name: 'Ava',
+    last_name: 'Gregoraci',
+    birthday: '1985-07-23',
+    phone: [
+      { type: 'mobile', number: '415-907-2647' },
+      { type: 'fax', number: '415-907-2647' }
+    ]
+  },
+  {
+    id: uuid(),
+    addresses: [
+      {
+        country: 'USA',
+        state: 'Nevada',
+        city: 'Las Vegas',
+        street: '1798  Hickory Ridge Drive'
+      },
+      {
+        country: 'USA',
+        state: 'Nevada',
+        city: 'Las Vegas',
+        street: '1798  Hickory Ridge Drive'
+      }
+    ],
+    avatarUrl: '/static/images/avatars/avatar_8.png',
+    createdAt: 1523048400000,
+    email: 'emilee.simchenko@devias.io',
+    first_name: 'Emilee',
+    last_name: 'Simchenko',
+    birthday: '1983-05-29',
+    phone: [
+      { type: 'mobile', number: '702-661-1654' },
+      { type: 'fax', number: '702-661-1654' }
+    ]
+  },
+  {
+    id: uuid(),
+    addresses: [
+      {
+        country: 'USA',
+        state: 'Michigan',
+        city: 'Detroit',
+        street: '3934  Wildrose Lane'
+      },
+      {
+        country: 'USA',
+        state: 'Michigan',
+        city: 'Detroit',
+        street: '3934  Wildrose Lane'
+      }
+    ],
+    avatarUrl: '/static/images/avatars/avatar_9.png',
+    createdAt: 1554702800000,
+    email: 'kwak.seong.min@devias.io',
+    first_name: 'Kwak',
+    last_name: 'Seong-Min',
+    birthday: '2000-02-31',
+    phone: [
+      { type: 'mobile', number: '313-812-8947' },
+      { type: 'fax', number: '313-812-8947' }
+    ]
+  },
+  {
+    id: uuid(),
+    addresses: [
+      {
+        country: 'USA',
+        state: 'Utah',
+        city: 'Salt Lake City',
+        street: '368 Lamberts Branch Road'
+      },
+      {
+        country: 'USA',
+        state: 'Utah',
+        city: 'Salt Lake City',
+        street: '368 Lamberts Branch Road'
+      }
+    ],
+    avatarUrl: '/static/images/avatars/avatar_10.png',
+    createdAt: 1522702800000,
+    email: 'merrile.burgett@devias.io',
+    first_name: 'Merrile',
+    last_name: 'Burgett',
+    birthday: '1994-01-22',
+    phone: [
+      { type: 'mobile', number: '801-301-7894' },
+      { type: 'fax', number: '801-301-7894' }
+    ]
+  }
+];
